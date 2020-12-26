afed9e90a30c65558c969fde260c170d1f9f8d18
Jacob Haigh <jacobhaigh@fb.com>
2020-12-25 17:22:29 -0800

    Created react app

---
+++ goose/src/App.js
@@ -0,0 +1,31 @@
+// @flow strict
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+type Props = {
+  name: string
+}
+function App(props: Props):React$Node {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> {props.name}.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
