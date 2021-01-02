f3d5e168a473676e19c7c3c160d5cc60536b4354
Jacob Haigh <jacobhaigh@fb.com>
2021-01-01 20:36:22 -0800

    flow types for jest global

--- goose/src/app/__tests__/App.test.js
+++ goose/src/app/__tests__/App.test.js
@@ -1,8 +1,10 @@
+// @flow strict-local
+import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+import App from "../App.react";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(<App name="My Name" />);
   const linkElement = screen.getByText(/LearAAAAn react/i);
   expect(linkElement).toBeInTheDocument();
 });
