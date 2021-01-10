f1137e8e9def32e9594a3b613bd8e095b06de975
Jacob Haigh <jacobhaigh@fb.com>
2021-01-10 09:07:25 -0800

    ran prettier on firebasejs

--- goose/src/app/firebase.js
+++ goose/src/app/firebase.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsfd5hrTR1cI0QPvMeueX1D-izWX5aduU",
@@ -6,7 +6,7 @@ const firebaseConfig = {
   projectId: "naivegoose",
   storageBucket: "naivegoose.appspot.com",
   messagingSenderId: "118417357060",
-  appId: "1:118417357060:web:e7cf75d4c3fe147614b851"
+  appId: "1:118417357060:web:e7cf75d4c3fe147614b851",
 };
 
 firebase.initializeApp(firebaseConfig);
